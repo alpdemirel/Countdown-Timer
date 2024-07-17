55b9382ed6c5195679e53d234f716f40fe578430
alpdemirel <alpdemirel21@ku.edu.tr>
2024-07-17 15:33:41 +0300

    Reduced buttons to two

--- script.js
+++ script.js
@@ -1,64 +1,47 @@
 let interval;
 let remainingTime;
 let isRunning = false;
+let isPaused = false;
 
 function startTimer() {
-    if (isRunning) return;
-
-    const hours = parseInt(document.getElementById('hour').value) || 0;
-    const mins = parseInt(document.getElementById('min').value) || 0;
-    const secs = parseInt(document.getElementById('sec').value) || 0;
-
-    remainingTime = (hours * 3600) + (mins * 60) + secs;
-
-    const countdownElement = document.getElementById('countdown');
-
-    interval = setInterval(function() {
-        if (remainingTime <= 0) {
-            clearInterval(interval);
-            countdownElement.innerHTML = "Time's up!";
-            isRunning = false;
-            return;
+    if (isRunning && !isPaused) {
+        stopTimer();
+    } else {
+        if (!isPaused) {
+            const hours = parseInt(document.getElementById('hour').value) || 0;
+            const mins = parseInt(document.getElementById('min').value) || 0;
+            const secs = parseInt(document.getElementById('sec').value) || 0;
+            remainingTime = (hours * 3600) + (mins * 60) + secs;
         }
 
-        const hourDisp = Math.floor(remainingTime / 3600);
-        const minDisp = Math.floor((remainingTime % 3600) / 60);
-        const secDisp = remainingTime % 60;
+        const countdownElement = document.getElementById('countdown');
 
-        countdownElement.innerHTML = `${hourDisp}h ${minDisp}m ${secDisp}s`;
-        remainingTime--;
-    }, 1000);
+        interval = setInterval(function() {
+            if (remainingTime <= 0) {
+                clearInterval(interval);
+                countdownElement.innerHTML = "Time's up!";
+                isRunning = false;
+                isPaused = false;
+                return;
+            }
 
-    isRunning = true;
+            const hourDisp = Math.floor(remainingTime / 3600);
+            const minDisp = Math.floor((remainingTime % 3600) / 60);
+            const secDisp = remainingTime % 60;
+
+            countdownElement.innerHTML = `${hourDisp}h ${minDisp}m ${secDisp}s`;
+            remainingTime--;
+        }, 1000);
+
+        isRunning = true;
+        isPaused = false;
+    }
 }
 
 function stopTimer() {
     clearInterval(interval);
-    isRunning = false;
-}
-
-function resumeTimer() {
-    if (isRunning) return;
-
-    const countdownElement = document.getElementById('countdown');
-
-    interval = setInterval(function() {
-        if (remainingTime <= 0) {
-            clearInterval(interval);
-            countdownElement.innerHTML = "Time's up!";
-            isRunning = false;
-            return;
-        }
-
-        const hourDisp = Math.floor(remainingTime / 3600);
-        const minDisp = Math.floor((remainingTime % 3600) / 60);
-        const secDisp = remainingTime % 60;
-
-        countdownElement.innerHTML = `${hourDisp}h ${minDisp}m ${secDisp}s`;
-        remainingTime--;
-    }, 1000);
-
     isRunning = true;
+    isPaused = true;
 }
 
 function resetTimer() {
@@ -69,4 +52,5 @@ function resetTimer() {
     document.getElementById('countdown').innerHTML = '';
     remainingTime = 0;
     isRunning = false;
+    isPaused = false;
 }
